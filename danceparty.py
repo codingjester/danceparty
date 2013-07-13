b561738d476271ce42cfebe174e7969d3a2d0ee2
John Bunting <codingjester@gmail.com>
2013-07-13 13:12:23 -0400

    Releasing a silly bit of code I wrote a year ago

---
+++ danceparty.py
@@ -0,0 +1,54 @@
+from flask import Flask
+from flask import render_template
+from flask import jsonify
+import simplejson as json
+import rdio
+import urllib2
+
+app = Flask(__name__)
+
+CONSUMER_KEY = "consumer_key"
+SECRET_KEY = "secret_key"
+
+r = rdio.Api(CONSUMER_KEY, SECRET_KEY)
+app.debug = True
+
+@app.route("/")
+def index():
+    return render_template('index.html')
+
+@app.route("/party")
+def party():
+    return render_template('party.html')  
+
+@app.route('/rdio')
+def rdio():
+    return "Soon."
+
+@app.route('/soundcloud')
+def soundcloud():
+    return render_template('soundcloud.html')
+
+@app.route('/spotify')
+def spotify():
+    return "Very soon."
+
+@app.route("/search/<trackname>")
+def search(trackname):
+    #Make ajax search and data return
+    data = r.search(query=trackname, types=["Track"])
+    return jsonify({'tracks': [{"name": track.name, "key": track.key, "artist": track.artist_name} for track in data.results]})
+
+@app.route("/getgifs")
+def gifs():
+    tags = ['gif']
+    import random
+    tag = random.choice(tags)
+    response = urllib2.urlopen("http://api.tumblr.com/v2/tagged?tag="+tag+"&api_key=<tumblr_consumer_key>")    
+    body = json.loads(response.read())
+    gifs = [gif['photos'][0]['original_size'] for gif in body['response'] if gif['type'] == 'photo']
+    body = {"data" : gifs}
+    return jsonify(**body)
+
+if __name__ == "__main__":
+    app.run()
